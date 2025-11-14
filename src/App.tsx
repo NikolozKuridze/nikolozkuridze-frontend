03e789f0cfe4cf4aad53b16e96d6370a4c6ecbdd
Nikoloz Kuridze <67338541+NikolozKuridze@users.noreply.github.com>
2025-11-14 23:47:48 +0400

    Revert "Build multilingual admin panel with blog system"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './hooks/useTheme';
 import { Navigation } from './components/Navigation/Navigation';
 import { Hero } from './sections/Hero/Hero';
@@ -7,99 +6,22 @@ import { Experience } from './sections/Experience/Experience';
 import { Projects } from './sections/Projects/Projects';
 import { Contact } from './sections/Contact/Contact';
 import { Footer } from './components/Footer/Footer';
-import AdminLogin from './pages/admin/Login';
-import AdminDashboard from './pages/admin/Dashboard';
-import AdminBlogs from './pages/admin/Blogs';
-import BlogEditor from './pages/admin/BlogEditor';
-import AdminProjects from './pages/admin/Projects';
-import ProjectEditor from './pages/admin/ProjectEditor';
-import ProtectedRoute from './components/admin/ProtectedRoute';
 import './styles/globals.css';
-
-function HomePage() {
-  return (
-    <div className="app">
-      <Navigation />
-      <main>
-        <Hero />
-        <About />
-        <Experience />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
-  );
-}
 
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<HomePage />} />
-
-          {/* Admin Routes */}
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route
-            path="/admin/dashboard"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/blogs"
-            element={
-              <ProtectedRoute>
-                <AdminBlogs />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/blogs/new"
-            element={
-              <ProtectedRoute>
-                <BlogEditor />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/blogs/edit/:id"
-            element={
-              <ProtectedRoute>
-                <BlogEditor />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/projects"
-            element={
-              <ProtectedRoute>
-                <AdminProjects />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/projects/new"
-            element={
-              <ProtectedRoute>
-                <ProjectEditor />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/projects/edit/:id"
-            element={
-              <ProtectedRoute>
-                <ProjectEditor />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <div className="app">
+        <Navigation />
+        <main>
+          <Hero />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
